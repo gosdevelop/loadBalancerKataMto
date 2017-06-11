6e8ef8b67c31c17385c11dda0fdac4f22e7b1d29
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-11 18:37:31 +0200

    filling server partially

--- src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -4,4 +4,10 @@ package edu.iis.mto.serverloadbalancer;
  * Created by Konrad Gos on 11.06.2017.
  */
 public class Vm {
+    public int size;
+
+    public Vm(int size) {
+        super();
+        this.size = size;
+    }
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -33,6 +33,16 @@ public class ServerLoadBalancerTest {
 		assertThat("server should cotain the vm", theServer.contains(theVm));
 	}
 
+	@Test
+	public void balancingServerWithTenSlotCapacity_andOneSlotVm_fillsTheServerWithTenPercent(){
+		Server theServer = a(server().withCapacity(10));
+		Vm theVm = a(vm().ofSize(1));
+		balancing(aServersListWith(theServer), aVmsListWith(theVm));
+
+		assertThat(theServer, hasCurrentLoadOf(10.0d));
+		assertThat("server should cotain the vm", theServer.contains(theVm));
+	}
+
 	private Vm[] aVmsListWith(Vm... vms) {
 		return vms;
 	}
