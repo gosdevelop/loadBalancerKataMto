7874f69a8b5db4cafda8fe74af3a3fb56ee50cfc
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-13 01:20:00 +0200

    filling server partially

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,11 +3,14 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
 
 	public double currentLoadPecentage;
+    public int capacity;
 
-	public Server(int capacity) {
-	}
+    public Server(int capacity) {
+        this.capacity = capacity;
+    }
 
     public boolean contains(Vm theVm) {
 		return true;
     }
+
 }
