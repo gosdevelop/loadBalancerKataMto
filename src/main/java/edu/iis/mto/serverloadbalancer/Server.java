a5aaa71c2bc819d10d52dbcb9893bb04a9882581
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-12 23:07:47 +0200

    refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
  * Created by Konrad Gos on 12.06.2017.
  */
 public class Server {
+    public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
 
@@ -13,5 +14,9 @@ public class Server {
 
     public Server(int capacity) {
         this.capacity = capacity;
+    }
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage = (double) vm.size / (double) capacity * MAXIMUM_LOAD;
     }
 }
