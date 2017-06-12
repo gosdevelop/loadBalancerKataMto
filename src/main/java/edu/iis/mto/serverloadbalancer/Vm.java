a926412d79e51bd9b8f56c86c2b1e5f62cd9c135
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-12 23:02:45 +0200

    filling server partially

--- src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -4,4 +4,9 @@ package edu.iis.mto.serverloadbalancer;
  * Created by Konrad Gos on 12.06.2017.
  */
 public class Vm {
+    public int size;
+
+    public Vm(int size) {
+        this.size = size;
+    }
 }
