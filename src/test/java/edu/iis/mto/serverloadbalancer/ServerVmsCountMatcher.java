6dc28f06e1ef7939e4e1223505b8145bf2bf728b
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-12 23:35:06 +0200

    adding more than one vms to server

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmsCountMatcher.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by Konrad Gos on 12.06.2017.
+ */
+public class ServerVmsCountMatcher extends TypeSafeMatcher<Server> {
+    private int expectedVmsCount;
+
+    public ServerVmsCountMatcher(int expectedVmsCount) {
+        this.expectedVmsCount = expectedVmsCount;
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("a server with vms count of ").appendValue(item.countVms());
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return expectedVmsCount == server.countVms();
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("a server with vms count of ").appendValue(expectedVmsCount);
+    }
+}
