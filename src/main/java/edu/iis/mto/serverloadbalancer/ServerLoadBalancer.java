b17a15e42a020644570977f1c55e2be33696d17d
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-13 12:12:23 +0200

    balancing zero vms impl

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -0,0 +1,9 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by Konrad Gos on 13.06.2017.
+ */
+public class ServerLoadBalancer {
+    public void balance(Server[] servers, Vm[] vms) {
+    }
+}
---
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -0,0 +1,7 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by Konrad Gos on 13.06.2017.
+ */
+public class Vm {
+}
