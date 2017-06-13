bc4812a569370607079f04ae79068f7c8bd89ab6
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-13 02:39:54 +0200

    over fitted vm cannot be located on server

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -70,6 +70,15 @@ public class ServerLoadBalancerTest {
 		assertThat("more loaded server should not contain the vm", !moreLoadedServer.contains(theVm));
 	}
 
+	@Test
+	public void balancingServerWithNotEnoughRoom_shouldNotBeFilledWithTheVm() {
+		Server theServer = a(server().withCapacity(10).withCurrentLoadOf(90.0d));
+		Vm theVm = a(vm().ofSize(2));
+
+		balance(aListOfServersWith(theServer), aVmListWith(theVm));
+		assertThat("server should not contain the vm", !theServer.contains(theVm));
+	}
+
 	private Vm[] aVmListWith(Vm... vms) {
 		return vms;
 	}
