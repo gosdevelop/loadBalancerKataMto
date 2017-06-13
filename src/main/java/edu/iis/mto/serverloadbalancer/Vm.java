0ac3a662f44db808f1bc1ddcf39dca5eac95a7bc
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-13 13:01:10 +0200

    refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -4,9 +4,13 @@ package edu.iis.mto.serverloadbalancer;
  * Created by Konrad Gos on 13.06.2017.
  */
 public class Vm {
-    public int size;
+    private int size;
 
     public Vm(int size) {
         this.size = size;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
