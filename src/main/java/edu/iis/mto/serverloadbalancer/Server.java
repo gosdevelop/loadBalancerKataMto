5294bdc90e68cec51f2a0407fd3b275cd9fd1538
Konrad.Gos <194948@edu.p.lodz.pl>
2017-06-13 12:43:12 +0200

    adding more than one vms to server

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Konrad Gos on 13.06.2017.
  */
@@ -7,6 +10,8 @@ public class Server {
     public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
+
+    List<Vm> vms = new ArrayList<Vm>();
 
     public boolean contains(Vm theVm) {
         return true;
@@ -18,5 +23,10 @@ public class Server {
 
     public void addVm(Vm vm) {
         currentLoadPercentage = (double) vm.size / (double) capacity * MAXIMUM_LOAD;
+        vms.add(vm);
+    }
+
+    public int countVms() {
+        return vms.size();
     }
 }
